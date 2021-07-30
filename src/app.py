143a01bfefe9b9c7379900f3dce5b54d8962c1ae
hang333 <jonathan.goh333@gmail.com>
2021-07-31 04:52:41 +0800

    Remove boilerplate content.

--- src/app.py
+++ src/app.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Depends
 import uvicorn
 
 from auth.jwt_bearer import JWTBearer
-from routes.student import router as StudentRouter
 from routes.admin import router as AdminRouter
 from routes.post import router as PostRouter
 
@@ -16,7 +15,6 @@ async def read_root():
 
 
 app.include_router(AdminRouter, tags=["Administrator"], prefix="/admin")
-app.include_router(StudentRouter, tags=["Students"], prefix="/student", dependencies=[Depends(token_listener)])
 app.include_router(PostRouter, tags=["Post"], prefix="/post")
 
 if __name__ == '__main__':
